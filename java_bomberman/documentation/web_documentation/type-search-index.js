a241a23104d612065912116b5877da771994878c
orlowskik <104775955+orlowskik@users.noreply.github.com>
2023-06-25 11:56:15 +0200

    Add files via upload

---
+++ java_bomberman/documentation/web_documentation/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"p":"<Unnamed>","l":"Aggressive"},{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"Board"},{"p":"<Unnamed>","l":"Bomb"},{"p":"<Unnamed>","l":"Cowardly"},{"p":"<Unnamed>","l":"Enemy"},{"p":"<Unnamed>","l":"Freaky"},{"p":"<Unnamed>","l":"Player"}];updateSearchResults();
